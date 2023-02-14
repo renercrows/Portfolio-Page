0af33f1075fe6979d71578473f02cc2cf20a5af7
renercrows <ungredda95@gmail.com>
2023-02-14 19:43:25 -0400

    Add portfolio page

---
+++ script.js
@@ -0,0 +1,297 @@
+const projectCards = [
+  {
+    id: 1,
+    title: 'Blue Dreams Yatch',
+    img: 'images/Blue-Dreams.jfif',
+    description: 'A rental webpage where you can do booking of diferent yatchs, setup the city where you want it, see reservations and deleted. This project was made using React, Redux, Rails and API. It is a five people project, so do not hesitate to visit Daniel Gonzales repository and check out the project.',
+    tags: ['React', 'Ruby on rails', 'API', 'Authentification'],
+    link: 'https://develop.d394vyctawdkiv.amplifyapp.com/',
+    source: 'https://github.com/dgonzalesi/blue-dreams-front-end',
+  },
+  {
+    id: 2,
+    title: 'Pokedex-API',
+    img: 'images/Pokedex.png',
+    description: 'This page was made using mainly React and Redux. This project connects to a Pokedex API using a Pokemon gallery, see your favorites Pokemon of the first generation using the searcher bar at the top-middle of the page, and also can check the stats and attacks of the selected Pokemon. Feel free to come and check it out.',
+    tags: ['React', 'API'],
+    link: 'https://63ebfb31fcaaba1c0da1bd0f--extraordinary-buttercream-fbbff9.netlify.app/',
+    source: 'https://github.com/renercrows/Pokedex-API',
+  },
+  {
+    id: 3,
+    title: 'Concert-Page',
+    img: 'images/Concert.png',
+    description: 'A webpage of a concert with different singers, this is a concert page to check out the new concert event. It was developed with HTML, CSS and JavaScript, using DOM as the main JavaScript code. Do not hesitate to come and see the project.',
+    tags: ['html', 'CSS', 'JavaScript'],
+    link: 'https://renercrows.github.io/Concert-Page/',
+    source: 'https://github.com/renercrows/Concert-Page',
+  },
+  {
+    id: 4,
+    title: 'Book-Store',
+    img: 'images/Book.png',
+    description: 'About This webpage was built using mainly React and Redux. This project displays the Book store, setting the user title of the book and author and categorizing it. You can post your inputs through the API. Feel free to check it out.',
+    tags: ['Ruby on Rails', 'Bootstrap', 'Redux'],
+    link: 'https://63ec08f0fcaaba26a5a1c021--beautiful-cat-a84a46.netlify.app/',
+    source: 'https://github.com/renercrows/Book-Store',
+  },
+];
+
+const projectSection = document.querySelector('.recent-works');
+
+function createCard(project) {
+  const pCard = document.createElement('article');
+  pCard.classList.add('work-card');
+
+  const img = document.createElement('img');
+  img.classList.add('image-placeholder');
+  img.setAttribute('src', project.img);
+  img.setAttribute('alt', 'Project thumbnail');
+  pCard.appendChild(img);
+
+  if (project.id % 2 === 0) {
+    pCard.classList.add('left-block');
+    img.classList.add('left-side');
+  }
+
+  const pContent = document.createElement('div');
+  pContent.classList.add('work-content');
+  pCard.appendChild(pContent);
+
+  const pHeader = document.createElement('h3');
+  pHeader.classList.add('work-header');
+  pHeader.textContent = project.title;
+  pContent.appendChild(pHeader);
+
+  const pText = document.createElement('p');
+  pText.classList.add('work-p');
+  pText.textContent = 'This is one of the four projects presented, check out the "See Project" button to see more about the project.';
+  pContent.appendChild(pText);
+
+  const pTags = document.createElement('ul');
+  pTags.classList.add('tags-section');
+  pContent.appendChild(pTags);
+
+  project.tags.forEach((_element, index) => {
+    const tag = document.createElement('li');
+    tag.classList.add('tag');
+    pTags.appendChild(tag);
+
+    const anchorTag = document.createElement('a');
+    anchorTag.setAttribute('href', '#');
+
+    anchorTag.textContent = project.tags[index];
+    tag.appendChild(anchorTag);
+  });
+
+  const pBtn = document.createElement('button');
+  pBtn.setAttribute('type', 'button');
+  pBtn.setAttribute('id', project.id);
+  pBtn.classList.add('work-card-button');
+  pBtn.textContent = 'See Project';
+
+  pContent.appendChild(pBtn);
+
+  projectSection.appendChild(pCard);
+}
+
+projectCards.forEach(createCard);
+
+const modalContainer = document.getElementById('modal-container');
+
+function closeModal() {
+  modalContainer.classList.remove('show');
+}
+
+function createModal(id) {
+  // Search the project with the same id in the projects array
+  const project = projectCards.find((project) => Number(project.id) === Number(id));
+  const modal = document.createElement('div');
+  modal.setAttribute('id', 'modal');
+  modalContainer.appendChild(modal);
+
+  const modalHeaderContainer = document.createElement('div');
+  modalHeaderContainer.classList.add('modal-header-container');
+  modal.appendChild(modalHeaderContainer);
+
+  const modalHeader = document.createElement('h3');
+  modalHeader.classList.add('modal-header');
+  modalHeader.textContent = project.title;
+  modalHeaderContainer.appendChild(modalHeader);
+
+  const modalClose = document.createElement('div');
+  modalClose.classList.add('modal-close');
+  modalHeaderContainer.appendChild(modalClose);
+  modalClose.addEventListener('click', closeModal);
+
+  for (let i = 0; i < 3; i += 1) {
+    const closeSpan = document.createElement('span');
+    closeSpan.classList.add('line');
+    modalClose.appendChild(closeSpan);
+  }
+
+  const modalImg = document.createElement('img');
+  modalImg.classList.add('modal-image');
+  modalImg.setAttribute('src', project.img);
+  modalImg.setAttribute('alt', 'Project thumbnail');
+  modal.appendChild(modalImg);
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modal.appendChild(modalContent);
+
+  const modalP = document.createElement('p');
+  modalP.classList.add('modal-p');
+  modalP.textContent = project.description;
+  modalContent.appendChild(modalP);
+
+  const modalTags = document.createElement('ul');
+  modalTags.classList.add('tags-section');
+  modalContent.appendChild(modalTags);
+
+  project.tags.forEach((_element, index) => {
+    const tag = document.createElement('li');
+    tag.classList.add('tag');
+    modalTags.appendChild(tag);
+
+    const anchorTag = document.createElement('a');
+    anchorTag.setAttribute('href', '#');
+
+    anchorTag.textContent = project.tags[index];
+    tag.appendChild(anchorTag);
+  });
+
+  const modalBtnContainer = document.createElement('div');
+  modalBtnContainer.setAttribute('id', 'modal-btn-container');
+  modalContent.appendChild(modalBtnContainer);
+
+  const liveBtn = document.createElement('a');
+  liveBtn.setAttribute('id', 'live');
+  liveBtn.setAttribute('href', project.link);
+  liveBtn.textContent = 'See Live';
+  const liveIcon = document.createElement('img');
+  liveIcon.setAttribute('src', 'images/live-icon.svg');
+  liveIcon.setAttribute('alt', 'Arrow link icon');
+  liveBtn.appendChild(liveIcon);
+  modalBtnContainer.appendChild(liveBtn);
+  const srcBtn = document.createElement('a');
+  srcBtn.setAttribute('id', 'source');
+  srcBtn.setAttribute('href', project.source);
+  srcBtn.textContent = 'See Source';
+  const srcIcon = document.createElement('img');
+  srcIcon.setAttribute('src', 'images/githubicon.png');
+  srcIcon.setAttribute('alt', 'Github icon');
+  srcBtn.appendChild(srcIcon);
+  modalBtnContainer.appendChild(srcBtn);
+}
+
+function cleanModal() {
+  const div = document.getElementById('modal-container');
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+}
+
+function openModal() {
+  modalContainer.classList.add('show');
+  window.onclick = (event) => {
+    if (event.target === modalContainer) {
+      modalContainer.classList.remove('show');
+    }
+  };
+}
+
+projectSection.addEventListener('click', (e) => {
+  const elementId = e.target.id;
+  // Only for button See project
+  if (e.target.classList.contains('work-card-button')) {
+    cleanModal(modalContainer.id);
+    createModal(elementId);
+    openModal();
+  }
+});
+
+const hmgIcon = document.querySelector('.hmg-menu');
+const overlayMenu = document.querySelector('.overlay-menu');
+const closeBtn = document.querySelector('.close');
+
+hmgIcon.addEventListener('click', () => {
+  hmgIcon.classList.toggle('active');
+  overlayMenu.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  hmgIcon.classList.toggle('active');
+  overlayMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.link').forEach((n) => n
+  .addEventListener('click', () => {
+    hmgIcon.classList.remove('active');
+    overlayMenu.classList.remove('active');
+  }));
+
+document.querySelectorAll('.close').forEach((n) => n
+  .addEventListener('click', () => {
+    hmgIcon.classList.remove('active');
+    overlayMenu.classList.remove('active');
+  }));
+
+// Contact Form Validation
+
+const submitBtn = document.getElementById('submit-btn');
+const error = document.getElementById('error-msg');
+const emailInput = document.getElementById('email-at');
+
+error.style.color = 'red';
+error.style.padding = '0.5em';
+error.style.textAlign = 'center';
+
+submitBtn.onclick = (event) => {
+  const emailValue = document.getElementById('email-at').value;
+  const lowerEmail = emailValue.toLowerCase();
+
+  if (emailValue !== lowerEmail) {
+    error.textContent = 'Please check the email format';
+    emailInput.classList.toggle('email-error-msg');
+    event.preventDefault();
+  }
+};
+
+// LocalStorage input data preservation
+const inputFields = document.getElementById('contact');
+const nameInput = document.getElementById('name-input');
+const msgInput = document.getElementById('msg-input');
+
+let inputData;
+
+inputFields.addEventListener('input', () => {
+  const nameValue = nameInput.value;
+  const email = emailInput.value;
+  const msg = msgInput.value;
+
+  // make email format error disappear with new entry
+  error.textContent = '';
+  emailInput.classList.remove('email-error-msg');
+
+  if (!nameValue && !email && !msg) {
+    return;
+  }
+
+  inputData = {
+    nameValue,
+    email,
+    msg,
+  };
+
+  localStorage.setItem('inputData', JSON.stringify(inputData));
+});
+
+inputData = JSON.parse(localStorage.getItem('inputData'));
+
+// Data Store in localStorage
+if (inputData) {
+  nameInput.value = inputData.nameValue;
+  emailInput.value = inputData.email;
+  msgInput.value = inputData.msg;
+}
